b635eca096a7b891528533a8d3d9971d1e895ea3
Chance Snow <git@chancesnow.me>
2016-10-16 07:16:59 -0700

    Retrieve port from env

--- Main.hs
+++ Main.hs
@@ -2,5 +2,12 @@ module Main where
 
 import App
 
+import System.Environment
+import System.IO
+
 main :: IO ()
-main = run 8080
+main = do
+    hSetBuffering stdout LineBuffering
+    env <- getEnvironment
+    let port = maybe 8080 read $ lookup "PORT" env
+    run 8080
